0cc29b6bb9dff691694a8ae531a181dc43840165
Samuel Varga <sam@vargasd.com>
2025-03-15 09:11:41 -0400

    feat(home-manager): add home-manager

--- flake.nix
+++ flake.nix
@@ -5,11 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
       self,
+      home-manager,
       nixpkgs,
       nix-darwin,
     }:
@@ -17,6 +20,15 @@
       darwinConfigurations."sams-Mac-mini" = nix-darwin.lib.darwinSystem {
         modules = [
           ./nix-darwin
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.sam = import ./home-manager/default.nix;
+
+            # Why did I need this?
+            users.users.sam.home = "/Users/sam";
+          }
         ];
       };
     };
---
+++ home-manager/default.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  home.stateVersion = "25.05";
+
+  home.username = "sam";
+  home.homeDirectory = "/Users/sam";
+
+  programs.home-manager.enable = true;
+}
