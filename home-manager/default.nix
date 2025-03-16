82c34f64ef51581d300b6a73ce5b1d30b6e27e8a
Samuel Varga <sam@vargasd.com>
2025-03-16 14:45:46 -0400

    feat(home-manager): add lazygit config

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -203,6 +203,16 @@
     };
   };
 
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git.paging = {
+        colorArg = "always";
+        pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format=\"lazygit-edit://{path}:{line}\"";
+      };
+    };
+  };
+
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
