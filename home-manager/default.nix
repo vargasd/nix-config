99e30baee125e7363868ca46bb401cce2458eb17
Samuel Varga <sam@varga.sh>
2025-04-02 11:02:22 -0400

    feat(home-manager): add skip hook prefix for lazygit

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -78,9 +78,12 @@ user:
   programs.lazygit = {
     enable = true;
     settings = {
-      git.paging = {
-        colorArg = "always";
-        pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format=\"lazygit-edit://{path}:{line}\"";
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format=\"lazygit-edit://{path}:{line}\"";
+        };
+        skipHookPrefix = "!";
       };
     };
   };
