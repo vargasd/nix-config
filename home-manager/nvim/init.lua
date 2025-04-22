eeec5b6ee67b8d40baa6e0f4a6a110d5b10aadbb
Samuel Varga <samuel.varga@sap.com>
2025-04-22 14:05:01 -0400

    refactor(home-manager): remove deprecated sign_define call

--- home-manager/nvim/init.lua
+++ home-manager/nvim/init.lua
@@ -2,14 +2,21 @@ require("config.options")
 require("config.lazy")
 require("config.keymap")
 
--- Diagnostics
-local signs = { Error = "●", Warn = "●", Hint = "●", Info = "●" }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "●",
+			[vim.diagnostic.severity.WARN] = "●",
+			[vim.diagnostic.severity.INFO] = "●",
+			[vim.diagnostic.severity.HINT] = "●",
+		},
+		numhl = {
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+		},
+	},
 	virtual_text = {
 		severity = {
 			min = vim.diagnostic.severity.WARN,
