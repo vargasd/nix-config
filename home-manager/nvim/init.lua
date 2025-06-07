461bf48a37baa683fb5b9a494375e9edf2a52d99
Samuel Varga <samuel.varga@sap.com>
2025-06-07 13:03:23 -0400

    chore: remove deprecated call

--- home-manager/nvim/init.lua
+++ home-manager/nvim/init.lua
@@ -35,7 +35,7 @@ vim.diagnostic.config({
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 	group = highlight_group,
 	pattern = "*",
