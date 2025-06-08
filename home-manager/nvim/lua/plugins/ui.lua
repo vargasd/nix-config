96f2b17122cbe78f5d15f66f2e97e8a45299238f
Samuel Varga <samuel.varga@sap.com>
2025-06-08 11:05:35 -0400

    refactor(nvim): make ufo lazy

--- home-manager/nvim/lua/plugins/ui.lua
+++ home-manager/nvim/lua/plugins/ui.lua
@@ -353,24 +353,39 @@ return {
 
 	{
 		"kevinhwang91/nvim-ufo",
-		event = "VeryLazy",
 		dependencies = { "kevinhwang91/promise-async" },
-		config = function()
-			local ufo = require("ufo")
-			ufo.setup({
-				provider_selector = function(bufnr, filetype, buftype)
-					return { "treesitter", "indent" }
-				end,
-			})
-
+		init = function()
 			vim.o.foldenable = true
 			vim.o.foldlevel = 99
 			vim.o.foldlevelstart = 99
-			vim.keymap.set("n", "zR", ufo.openAllFolds)
-			vim.keymap.set("n", "zM", ufo.closeAllFolds)
-			vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds)
-			vim.keymap.set("n", "zm", ufo.closeFoldsWith)
 		end,
+		opts = {},
+		keys = {
+			{
+				"zR",
+				function()
+					require("ufo").openAllFolds()
+				end,
+			},
+			{
+				"zM",
+				function()
+					require("ufo").closeAllFolds()
+				end,
+			},
+			{
+				"zr",
+				function()
+					require("ufo").openFoldsExceptKinds()
+				end,
+			},
+			{
+				"zm",
+				function()
+					require("ufo").closeFoldsWith()
+				end,
+			},
+		},
 	},
 
 	{
