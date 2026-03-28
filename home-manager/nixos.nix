b21b6a1be67eb37959e978596590e4d11fc9f837
Samuel Varga <sam@varga.sh>
2026-03-28 13:40:43 -0400

    feat(nixos): configure swayidle

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -430,4 +430,38 @@
     };
   };
 
+  services.swayidle =
+    let
+      lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
+      display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+      lockTime = 300;
+    in
+    {
+      enable = true;
+      timeouts = [
+        {
+          timeout = lockTime - 20;
+          command = "${pkgs.libnotify}/bin/notify-send 'Locking in 20 seconds' -t 20000";
+        }
+        {
+          timeout = lockTime;
+          command = lock;
+        }
+        {
+          timeout = 1200;
+          command = display "off";
+          resumeCommand = display "on";
+        }
+        {
+          timeout = 3600;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+      events = {
+        before-sleep = "${display "off"}; ${lock}";
+        after-resume = display "on";
+        lock = "${display "off"}; ${lock}";
+        unlock = display "on";
+      };
+    };
 }
