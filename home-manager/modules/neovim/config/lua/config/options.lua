aa3f335fff0a71720a5aaf7039ade1bd748cc5c0
Samuel Varga <sam@varga.sh>
2026-04-04 15:15:56 -0400

    chore: clean up old comments
    Hopefully never to be used again

--- home-manager/modules/neovim/config/lua/config/options.lua
+++ home-manager/modules/neovim/config/lua/config/options.lua
@@ -44,34 +44,5 @@ vim.o.ruler = false
 vim.opt.fillchars.stl = " "
 vim.opt.fillchars.stlnc = " "
 
--- --#region kitty/wezterm
 vim.o.laststatus = 0
 vim.o.statusline = " "
--- --#endregion kitty
---#region alacritty/ghostty/tmux
--- set title dynamimcally by buffer
--- local overridetitle = ""
--- -- vim.o.laststatus = 3
--- vim.opt.title = true
--- vim.opt.titlelen = 0
--- vim.api.nvim_create_autocmd("BufEnter", {
--- 	callback = function(args)
--- 		local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
--- 		if overridetitle == "" and buftype == "" then
--- 			vim.o.titlestring = vim.fn.expand("%:p:h:t")
--- 				.. "/"
--- 				.. vim.fn.expand("%:t")
--- 				.. " ("
--- 				.. vim.fn.expand("%:~:.:h:h")
--- 				.. ")"
--- 			-- else
--- 			-- 	vim.o.titlestring = buftype
--- 		end
--- 	end,
--- })
-
-vim.api.nvim_create_user_command("SetTitle", function(opts)
-	overridetitle = opts.fargs[1]
-	vim.o.titlestring = overridetitle
-end, { nargs = 1 })
---#endregion
