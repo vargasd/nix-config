4a8f6a063124c4c8ca0491e431e671722a2caa17
Samuel Varga <sam@varga.sh>
2026-04-24 14:17:08 -0400

    feat(zsh): add zmx session management

--- home-manager/modules/zsh/init.zsh
+++ home-manager/modules/zsh/init.zsh
@@ -17,10 +17,56 @@ setopt INC_APPEND_HISTORY
 
 setopt PROMPT_SUBST
 export PROMPT='
-%B%(?.%F{green}.%F{red})%~ %F{cyan}$(
-	git symbolic-ref --short HEAD 2> /dev/null || git rev-parse --short HEAD 2> /dev/null
+%B%(?.%F{green}.%F{red})%~%F{magenta}${ZMX_SESSION:+ $ZMX_SESSION} %F{cyan}$(
+  git symbolic-ref --short HEAD 2> /dev/null || git rev-parse --short HEAD 2> /dev/null
 )
 %F{yellow}→ %f%b'
+
+function s () {
+  if [[ -n "$ZMX_SESSION" && -n "$1" ]]; then
+    zmx attach "$(echo $ZMX_SESSION | cut -d# -f1)#$1"
+  else
+    local dir=$(zoxide query --interactive)
+    if [[ -n "$dir" ]]; then
+      zsh -c "cd ${(q)dir} && zmx attach ${(q)session}"
+    fi
+  fi
+}
+
+function zmx-session () {
+  local dir
+  dir=$(zoxide query --interactive </dev/tty)
+  if [[ -n "$dir" ]]; then
+    local session="${dir:t}#1"
+    BUFFER="zsh -c 'cd ${(q)dir} && zmx attach ${(q)session}'"
+    zle accept-line
+  else
+    zle reset-prompt
+  fi
+}
+zle -N .zmx-session zmx-session
+bindkey -M emacs "^T" .zmx-session
+
+function zmx-subsession () {
+  if [[ -n "$ZMX_SESSION" ]]; then
+    local n="${1:-${WIDGET##.zmx-subsession_}}"
+    zmx attach "$(echo $ZMX_SESSION | cut -d# -f1)#$n"
+  fi
+}
+for i in {1..9}; do
+  zle -N ".zmx-subsession_$i" zmx-subsession
+  bindkey -M emacs "\e$i" ".zmx-subsession_$i"
+done
+
+function zmx-scrollback () {
+  if [[ -n "$ZMX_SESSION" ]]; then
+    zle -I
+    zmx history "$ZMX_SESSION" --vt | nvim -c ':call nvim_open_term(0, #{})'
+    zle reset-prompt
+  fi
+}
+zle -N .zmx-scrollback zmx-scrollback
+bindkey -M emacs "\ez" .zmx-scrollback
 
 function xtitle () {
 		builtin print -n -- "\e]0;$PWD\a"
