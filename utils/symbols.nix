cde0e4567d3bc93f7216f71c3c20320921125fcb
Samuel Varga <sam@varga.sh>
2026-06-27 08:42:03 -0400

    bemoji: add non-emoji unicode chars

--- utils/symbols.nix
+++ utils/symbols.nix
@@ -17,13 +17,31 @@ let
     }
     |> builtins.readFile
     |> builtins.fromJSON;
+  unicodeData =
+    pkgs.fetchurl {
+      url = "https://unicode.org/Public/16.0.0/ucd/extracted/DerivedName.txt";
+      hash = "sha256-DMFGn6oMVRhXLvk/T0V/k6qKFgzjIKrTeT2F9LQ1/SQ=";
+    }
+    |> builtins.readFile;
 in
 {
   all =
     emojiData
-    ++
-      # TODO use https://unicode.org/Public/16.0.0/ucd/UnicodeData.txt or something else for symbols
-      [ ]
+    ++ (
+      unicodeData
+      |> pkgs.lib.splitString "\n"
+      |> builtins.filter (line: line != "" && !(pkgs.lib.strings.hasPrefix "#" line))
+      |> map (
+        line:
+        line
+        |> pkgs.lib.splitString ";"
+        |> (arr: {
+          label = builtins.elemAt arr 1 |> pkgs.lib.strings.trim;
+          # 🥴 https://discourse.nixos.org/t/how-can-i-put-an-nonprintable-character-in-a-nix-expression/47750/8
+          emoji = ''"\u${builtins.elemAt arr 0 |> pkgs.lib.strings.trim}"'' |> builtins.fromJSON;
+        })
+      )
+    )
     ++ (
       builtins.removeAttrs nerdData [ "METADATA" ]
       |> pkgs.lib.mapAttrsToList (
