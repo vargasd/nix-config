6c30f75668ccfddc8020fd830dde80f7a4b7432f
Samuel Varga <sam@varga.sh>
2026-07-17 12:00:19 -0400

    zsh: simplify config

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   ...
 }:
 {
@@ -9,16 +8,37 @@
     defaultKeymap = "emacs";
 
     history = {
-      append = false; # using INC_APPEND_HISTORY which home-manager doesn't support
+      append = false;
       ignoreAllDups = true;
       ignoreSpace = true;
-      save = 50000;
-      size = 50000;
       share = false;
     };
 
-    initContent = builtins.readFile ./init.zsh + ''
-      source ${pkgs.fzf-git-sh}/share/fzf-git-sh/fzf-git.sh
+    initContent = /* zsh */ ''
+      # shift-tab backwards through menu
+      bindkey '^[[Z' reverse-menu-complete
+      # completion menu highlighting
+      zstyle ':completion:*' menu select
+      # case-insensitive completion
+      zstyle ':completion:*' matcher-list '''''' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+
+      # better word navigation
+      autoload -U select-word-style
+      select-word-style bash
+
+      # bind [delete] for forward deletion
+      bindkey "^[[3~" delete-char
+      bindkey "^[3;5~" delete-char
+
+      autoload -z edit-command-line
+      zle -N edit-command-line
+      bindkey "^O" edit-command-line
+
+      setopt PROMPT_SUBST
+
+      export PROMPT='
+      %B%(?.%F{green}.%F{red})%~%F{magenta} $ZMX_SESSION
+      %F{yellow}→ %f%b'
     '';
   };
 }
