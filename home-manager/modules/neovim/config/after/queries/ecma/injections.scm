22644498d27fcc32b22099c088e51b9eb68a3a7f
Samuel Varga <sam@varga.sh>
2026-08-25 10:25:30 -0400

    neovim: fix prisma function injections

--- home-manager/modules/neovim/config/after/queries/ecma/injections.scm
+++ home-manager/modules/neovim/config/after/queries/ecma/injections.scm
@@ -4,9 +4,18 @@
 (call_expression
 	function: (member_expression
 		property: (property_identifier) @_prop
-		(#any-of? @_prop "sql" "$queryRaw" "$executeRaw" "$queryRawUnsafe" "$executeRawUnsafe"))
+		(#any-of? @_prop "sql" "$queryRaw" "$executeRaw"))
 	arguments: ((template_string) @injection.content
 		(#offset! @injection.content 0 1 0 -1)
-  (#set! injection.include-children)
-	(#set! injection.language "sql")))
-
+		(#set! injection.include-children)
+		(#set! injection.language "sql")))
+; and ones that can be functions
+(call_expression
+	function: (member_expression
+		property: (property_identifier) @_prop
+		(#any-of? @_prop "$queryRaw" "$executeRaw" "$queryRawUnsafe" "$executeRawUnsafe"))
+	arguments: (arguments
+		(template_string) @injection.content)
+	(#offset! @injection.content 0 1 0 -1)
+	(#set! injection.include-children)
+	(#set! injection.language "sql"))
