92475f1c5150e1b7551e6a07e277c4d73add2d58
Samuel Varga <sam@varga.sh>
2026-09-21 10:50:54 -0400

    work: add nix-ld

--- nixos/modules/work.nix
+++ nixos/modules/work.nix
@@ -10,6 +10,19 @@
     inputs.sentinelone.nixosModules.sentinelone
   ];
 
+  # https://wiki.nixos.org/wiki/Nix-ld needed for misbehaving node deps
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      ## Put here any library that is required when running a package
+      ## ...
+      ## Uncomment if you want to use the libraries provided by default in the steam distribution
+      ## but this is quite far from being exhaustive
+      ## https://github.com/NixOS/nixpkgs/issues/354513
+      # (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
+    ];
+  };
+
   services.sentinelone = {
     enable = true;
     sentinelOneManagementTokenPath = "/etc/secrets/sentinelone-key"; # TODO: Should use sops. Need to write token from pass in plaintext :/
